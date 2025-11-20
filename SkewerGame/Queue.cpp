f97cdf4e0582fe4855cc4b6e4cb007a20d0866c7
wlgus123 <mail30019794@gmail.com>
2025-11-20 16:23:10 +0900

    파일 삭제 및 큐 구현

---
+++ SkewerGame/Queue.cpp
@@ -0,0 +1,63 @@
+﻿#include "Queue.h"
+#include <iostream>
+
+using namespace std;
+
+// 생성자/소멸자
+Queue::Queue()
+{
+	this->m_size = 0;
+	this->m_queue = new int[QUEUE_MAX];
+}
+
+Queue::~Queue()
+{
+	delete[] this->m_queue;
+}
+
+// 원소 넣기
+void Queue::Push(int p_src)
+{
+	this->m_queue[this->m_rear] = p_src;
+	++this->m_rear;
+	++this->m_size;
+}
+
+// 원소 빼기
+void Queue::Pop(int p_src)
+{
+	if (this->m_size != 0)		// 큐가 비어있지 않을 때
+	{
+		this->m_queue[this->m_front] = 0;
+		++this->m_front;
+		--this->m_size;
+	}
+	else
+	{
+		cout << "큐가 비어있습니다." << endl;
+	}
+}
+
+// 큐의 제일 앞 값
+int Queue::GetFront()
+{
+	return this->m_queue[this->m_front];
+}
+
+// 큐의 제일 뒷 값
+int Queue::GetBack()
+{
+	return this->m_queue[this->m_rear - 1];
+}
+
+// 큐가 비어있는지 여부
+bool Queue::IsEmpty()
+{
+	return this->m_size == 0;
+}
+
+// 큐의 크기
+int Queue::Size()
+{
+	return this->m_size;
+}
---
+++ SkewerGame/Queue.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+// 큐 클래스 생성
+// TODO: 템플릿 사용하기
+#ifndef QUEUE_MAX
+#define QUEUE_MAX 100	// 큐 저장 최대 크기
+
+class Queue
+{
+	int* m_queue;	// 큐 배열
+	int m_size;		// 큐 크기
+	int m_front;	// 제일 앞 값
+	int m_rear;		// 제일 뒷 값
+
+public:
+	Queue();
+	virtual ~Queue();
+
+	void Push(int p_src);
+	void Pop(int p_src);
+	int GetFront();		// 큐의 제일 앞 값 가져오기 (먼저 빠지는 원소)
+	int GetBack();			// 큐의 제일 뒷 값 가져오기
+	bool IsEmpty();		// 큐가 비어있는지 여부
+	int Size();			// 큐의 크기 가져오기
+};
+
+#endif
